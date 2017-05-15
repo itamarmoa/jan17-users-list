1b86c20a99831d1b3fdc80e7f4e5874a7bd84706
itamarmoa <itamar.moa@gmail.com>
2017-05-15 17:30:21 +0300

    Added "bundle.js" & "package.json"

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Itamar on 5/15/2017.
+ */
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: "/dist",
+        filename: "bundle.js"
+    },
+    devtool: "source-map"
+};
